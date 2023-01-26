69719d5b957f4d6c07517954f8d0ffa23cf7e50a
CharlieBMF <marcinski.karol@gmail.com>
2023-01-26 12:44:53 +0100

    dashboard select

--- django/dashboard/dashboard/views.py
+++ django/dashboard/dashboard/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import  render
+import pandas as pd
+from django.shortcuts import render
 from django.urls import reverse
+import pyodbc
+import pandas as pd
+import sys
+sys.path.append('/home/pi/Scripts')
+from conf import machines_names
 
 
 def main(request):
+    cnxn = pyodbc.connect(
+        'DRIVER=FreeTDS;'
+        'SERVER=159.228.208.243;'
+        'PORT=1433;'
+        'DATABASE=mapsData;'
+        'UID=python;'
+        'PWD=Daicel@DSSE;Encrypt=no',
+        timeout=1
+    )
+    del cnxn
+    data = pd.read_sql("select * from tMachinesForMaps WHERE idLine=32", cnxn)
+    for machine in machines_names.keys():
+        pass
+        
     return render(request, 'dashboard_mct_new.html')
+
+
